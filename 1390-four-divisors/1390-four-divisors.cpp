9a2f4b10c23e13a39c86abaa39183b7ccfe0875a
Manya Garg <manya111203@gmail.com>
2026-01-04 14:55:13 +0530

    Time: 99 ms (22.17%), Space: 37.8 MB (9.85%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -5,17 +5,14 @@ public:
         int sum = 0;
         for (int i = 0; i < n; i++) {
             int count = 0; // reset count for next element
-            vector<int>
-                divisors; // reset/clear divisors vector for next element
+            vector<int> divisors; // reset/clear divisors vector for next element
             // for (int j = 1; j <= nums[i]; j++) {  // 1 to i e.g- for 21,
             // check from 1 to 21
-            for (int j = 1; j * j <= nums[i];
-                 j++) { // optimized  (check till perfect square)
+            for (int j = 1; j * j <= nums[i]; j++) { // optimized  (check till perfect square)
                 if (nums[i] % j == 0) {
                     divisors.push_back(j);
                     count++;
-                    if (j !=
-                        nums[i] / j) { /* to add pairs like 2 & 18 for 2 * 18 =
+                    if (j != nums[i] / j) { /* to add pairs like 2 & 18 for 2 * 18 =
                                        36 & avoid repetition (6 * 6 = 36 ) */
                         divisors.push_back(nums[i] / j);
                         count++;
@@ -23,7 +20,7 @@ public:
                 }
             }
             if (count != 4) {
-                break;
+                continue;
             } else if (count == 4) {
                 for (int val : divisors) {
                     sum = sum + val;
